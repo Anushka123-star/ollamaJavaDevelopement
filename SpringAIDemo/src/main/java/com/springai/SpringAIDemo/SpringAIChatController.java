024afc0f991bfdb53387777ff66e1c64acb44c5b
Anushka Srivastava <kmanushkasrivastava@gmail.com>
2025-04-20 22:46:46 +0530

    Simple Chat application - Controller

---
+++ SpringAIDemo/src/main/java/com/springai/SpringAIDemo/SpringAIChatController.java
@@ -0,0 +1,34 @@
+package com.springai.SpringAIDemo;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
+@RestController
+class ChatbotController {
+
+    private final ChatClient chatClient;
+
+    ChatbotController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        this.chatClient = chatClientBuilder.clone()
+                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
+                .build();
+    }
+
+    @PostMapping("/chat/{conversationId}")
+    String chat(@PathVariable String conversationId, @RequestBody String question) {
+        return chatClient
+                .prompt()
+                .user(question)
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId))
+                .call()
+                .content();
+    }
+
+}
