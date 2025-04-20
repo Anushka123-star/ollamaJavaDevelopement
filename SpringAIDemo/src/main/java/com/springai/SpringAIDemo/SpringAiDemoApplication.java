e54fa6615def122f723a4e8515053b5bec40c614
Anushka Srivastava <kmanushkasrivastava@gmail.com>
2025-04-20 22:36:11 +0530

    Simple chatbot application

--- SpringAIDemo/src/main/java/com/springai/SpringAIDemo/SpringAiDemoApplication.java
+++ SpringAIDemo/src/main/java/com/springai/SpringAIDemo/SpringAiDemoApplication.java
@@ -1,13 +1,20 @@
 package com.springai.SpringAIDemo;
 
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAiDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDemoApplication.class, args);
+    }
 
+    @Bean
+    ChatMemory chatHistory() {
+        return new InMemoryChatMemory();
+    }
 }
